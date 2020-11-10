82a3d12fbef66768c22df53645eb56904aaf4911
Takashi Yoneuchi <takashi.yoneuchi@shift-js.info>
2020-11-11 02:30:59 +0900

    :bug: enable required services

--- src/infrastructure/main.tf
+++ src/infrastructure/main.tf
@@ -5,6 +5,16 @@ provider "google" {
 data "google_project" "project" {
 }
 
+resource "google_project_service" "iam" {
+  service = "iam.googleapis.com"
+}
+
+# dns
+
+resource "google_project_service" "dns" {
+  service = "dns.googleapis.com"
+}
+
 resource "google_dns_managed_zone" "default" {
   project = var.project
 
